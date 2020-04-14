f06ca938fc6c5c6f70066c17b6eb47399f6b4382
Mikhail Niedre <mikhail.niedre@t-systems.com>
2020-04-14 15:33:35 +0300

    avoid SpringMVC usage

--- src/main/java/cane/brothers/CorsConfig.java
+++ src/main/java/cane/brothers/CorsConfig.java
@@ -1,9 +1,7 @@
 package cane.brothers;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 //import org.springframework.security.config.annotation.web.builders.WebSecurity;
 //import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,13 +9,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 /**
  * Created by cane
  */
-@Configuration
-@EnableWebMvc
-//public class CorsConfig extends WebMvcConfigurationSupport {
-public class CorsConfig implements WebMvcConfigurer {
+//@Configuration
+//@EnableWebMvc
+public class CorsConfig extends WebMvcConfigurationSupport {
+//public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("http://localhost:5000");
+        //registry.addMapping("/**").allowedMethods("*");
     }
 }
--- src/main/java/cane/brothers/SwaggerConfig.java
+++ src/main/java/cane/brothers/SwaggerConfig.java
@@ -2,8 +2,6 @@ package cane.brothers;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -13,13 +11,9 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-//import org.springframework.security.config.annotation.web.builders.WebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
 @Configuration
 @EnableSwagger2
-//public class SwaggerConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
-public class SwaggerConfig implements WebMvcConfigurer {
+public class SwaggerConfig {
 
     @Bean
     public Docket api() {
@@ -33,31 +27,12 @@ public class SwaggerConfig implements WebMvcConfigurer {
 
     private ApiInfo metaData() {
         return new ApiInfoBuilder()
-                .title("REST API")
-                .description("\"Spring Boot REST API for Online Store\"")
+                .title("Readme app REST API")
+                .description("\"Spring Boot REST API for Readme App\"")
                 .version("1.0.0")
                 .license("Apache License Version 2.0")
                 .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
-                .contact(new Contact("team", "https://springframework.guru/about/", "john@springfrmework.guru"))
+                .contact(new Contact("webcane", "http://webcane.github.io/", "webcane@yandex.ru"))
                 .build();
-    }
-
-//    @Override
-//    public void configure(WebSecurity web) throws Exception {
-//        web.ignoring().antMatchers("/v2/api-docs",
-//                "/configuration/ui",
-//                "/swagger-resources/**",
-//                "/configuration/security",
-//                "/swagger-ui.html",
-//                "/webjars/**");
-//    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
