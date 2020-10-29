bb6510ae6290fb254a0441573b75105124b353ac
Mikhail Niedre <mikhail.niedre@t-systems.com>
2020-10-29 23:42:06 +0300

    fix local security config

--- src/main/java/cane/brothers/sec/LocalSecurityConfig.java
+++ src/main/java/cane/brothers/sec/LocalSecurityConfig.java
@@ -6,16 +6,25 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.Arrays;
 
 /**
  * Created by cane
  */
 @Profile("dev")
-@Order(10)
+//@Order(10)
 @EnableWebSecurity
 public class LocalSecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -32,24 +41,35 @@ public class LocalSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
-        http
-                .csrf()
-                .disable()
-                .httpBasic()
+        // super.configure(http); // DO NOT UNCOMMENT IT
+        http.authorizeRequests()
+                .antMatchers("/", "/error", "/public/**", "/favicon/**",
+                        "/build/**", "/fonts/**", "/management/**", "/actuator/**")
+                    .permitAll()
+                .antMatchers("/tags", "/articles", "/user")
+                    .hasAnyRole("USER")
+                .anyRequest().authenticated()
                 .and()
-                // TODO
-//                .authorizeRequests()
-//                    .antMatchers("/tags", "/articles", "/user").hasRole("USER")
-//                    .antMatchers("/", "/error", "/public/**", "/favicon/**",
-//                            "/build/**", "/fonts/**", "/management/**", "/actuator/**").permitAll()
-//                    .anyRequest().authenticated()
-//                    .and()
                 .formLogin()
-                .loginProcessingUrl("/oauth2/authorization/github")
-                .permitAll()
+                    .loginProcessingUrl("/login")
+                    .permitAll()
                 .and()
                 .logout()
-                .deleteCookies("JSESSIONID");
+                    .deleteCookies("JSESSIONID")
+                    .permitAll()
+                .and()
+                .csrf()
+                    .disable()
+                .cors();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5000"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
