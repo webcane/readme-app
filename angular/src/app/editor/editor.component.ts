d0c6a8496f0e7c1d86ae7c9a4b5b6d784af458bb
cane <webcane@ya.ru>
2021-04-29 22:16:01 +0300

    publish article

--- angular/src/app/editor/editor.component.ts
+++ angular/src/app/editor/editor.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
+import { Article } from '@app/shared/model/article.model';
+import { ArticlesService } from '@app/shared/service/articles.service';
 
 @Component({
   selector: 'app-editor',
@@ -10,7 +12,8 @@ export class EditorComponent implements OnInit {
 
   public articleForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder,
+    public articleService: ArticlesService) {
   }
 
   ngOnInit(): void {
@@ -24,11 +27,12 @@ export class EditorComponent implements OnInit {
   }
 
   publish(): void {
-    console.log("publish article");
-
-    // this.myDataService.submitUpdate(this.articleForm.getRawValue())
-    //   .subscribe((): void => {
-    //     alert('Saved!');
-    //   });
+    console.log("publish article " + JSON.stringify(this.articleForm.getRawValue()));
+    
+    this.articleService
+      .create(this.articleForm.getRawValue() as Article)
+      .subscribe((): void => {
+        this.articleForm.reset();
+      });
   }
 }
