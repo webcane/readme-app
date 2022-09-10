5015ded5a07db7774a6f79f600680ef3891b0357
Mikhail Niedre <mikhail.niedre@quantori.com>
2022-09-10 20:33:10 +0200

    #26 fix swagger version

--- src/main/java/cane/brothers/OpenApiConfig.java
+++ src/main/java/cane/brothers/OpenApiConfig.java
@@ -3,10 +3,13 @@ package cane.brothers;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Contact;
-import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
@@ -17,10 +20,9 @@ import org.springframework.core.annotation.Order;
 @Order(2)
 @Profile("openapi")
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "readme", description = "Readme app springdoc open api",
-    contact = @Contact(name = "support", email = "webcane@yandex.ru")),
-    security = {@SecurityRequirement(name = OpenApiConfig.AUTH_SCHEME)})
-@SecurityScheme(name = OpenApiConfig.AUTH_SCHEME, type = SecuritySchemeType.HTTP, bearerFormat = "JWT", scheme = "Bearer")
+@OpenAPIDefinition(security = {@SecurityRequirement(name = OpenApiConfig.AUTH_SCHEME)})
+@SecurityScheme(name = OpenApiConfig.AUTH_SCHEME, type = SecuritySchemeType.HTTP, bearerFormat = "JWT", scheme =
+    "Bearer")
 public class OpenApiConfig {
 
   public static final String AUTH_SCHEME = "bearerAuth";
@@ -28,5 +30,24 @@ public class OpenApiConfig {
   /**
    * Constructor
    */
-  public OpenApiConfig() {}
+  public OpenApiConfig() {
+  }
+
+  @Bean
+  public OpenAPI readmeAppOpenAPI(@Value("${app.name}") String appName,
+                                  @Value("${app.version}") String appVersion,
+                                  @Value("${app.description}") String appDescription,
+                                  @Value("${app.email}") String appEmail,
+                                  @Value("${app.url}") String appUrl) {
+    return new OpenAPI()
+        .info(new Info()
+            .title(appName)
+            .description(appDescription)
+            // .termsOfService("readme app terms")
+            .version(appVersion)
+            .contact(new Contact()
+//                .name("API Support")
+                .url(appUrl)
+                .email(appEmail)));
+  }
 }
