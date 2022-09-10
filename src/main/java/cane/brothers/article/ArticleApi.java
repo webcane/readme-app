ec9ddaba3d80cbf2cb96db5fb4aa33f6e6d0df3e
Mikhail Niedre <mikhail.niedre@quantori.com>
2022-09-10 18:31:21 +0200

    #47 article api

---
+++ src/main/java/cane/brothers/article/ArticleApi.java
@@ -0,0 +1,55 @@
+package cane.brothers.article;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.Explode;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.enums.ParameterStyle;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import java.util.Collection;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+
+public interface ArticleApi {
+
+  @Operation(summary = "get all articles", responses = {
+      @ApiResponse(responseCode = "200", description = "All articles found",
+          content = @Content(array = @ArraySchema(schema = @Schema(implementation = ArticleView.class)))),
+      @ApiResponse(responseCode = "401", description = "Missing jwt token",
+          content = @Content(schema = @Schema(hidden = true)))})
+  ResponseEntity<List<ArticleView>> findAllArticles();
+
+  @Operation(summary = "filter articles by tag name", responses = {
+      @ApiResponse(responseCode = "200", description = "There are some articles found by tag name",
+          content = @Content(array = @ArraySchema(schema = @Schema(implementation = ArticleView.class)))),
+      @ApiResponse(responseCode = "400", description = "Missing tag name",
+          content = @Content(schema = @Schema(hidden = true))),
+      @ApiResponse(responseCode = "401", description = "Missing jwt token",
+          content = @Content(schema = @Schema(hidden = true))),
+      @ApiResponse(responseCode = "404", description = "No articles found by given tag name",
+          content = @Content(schema = @Schema(hidden = true)))})
+  ResponseEntity<List<ArticleView>> findArticlesByTagNames(
+      @Parameter(explode = Explode.TRUE,
+          name = "tags",
+          description = "Collection of tag names",
+          in = ParameterIn.QUERY,
+          required = true,
+          style = ParameterStyle.FORM) Collection<String> tags);
+
+  @Operation(summary = "post new article", responses = {
+      @ApiResponse(responseCode = "201", description = "New article was stored",
+          content = @Content(schema = @Schema(implementation = ArticleView.class))),
+      @ApiResponse(responseCode = "400", description = "For example missing or empty url",
+          content = @Content(schema = @Schema(hidden = true))),
+      @ApiResponse(responseCode = "401", description = "Missing jwt token",
+          content = @Content(schema = @Schema(hidden = true))),
+      @ApiResponse(responseCode = "409", description = "Article already exists",
+          content = @Content(schema = @Schema(hidden = true)))}
+  )
+  ResponseEntity<?> postArticle(@RequestBody(content = @Content(schema = @Schema(implementation = ArticleView.class)))
+                                ArticleForm request);
+}
--- src/main/java/cane/brothers/article/ArticleController.java
+++ src/main/java/cane/brothers/article/ArticleController.java
@@ -23,7 +23,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -32,7 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequestMapping("/articles")
-public class ArticleController {
+public class ArticleController implements ArticleApi {
 
   private final ArticleService svc;
 
@@ -41,35 +40,15 @@ public class ArticleController {
     this.svc = service;
   }
 
+  @Override
   @GetMapping(produces = {"application/json"})
-  @Operation(summary = "get all articles", responses = {
-      @ApiResponse(responseCode = "OK", description = "200 OK. All articles found",
-          content = @Content(array = @ArraySchema(schema = @Schema(implementation = ArticleView.class)))),
-      @ApiResponse(responseCode = "UNAUTHORIZED", description = "401 Unauthorized. Missing jwt token",
-          content = @Content(schema = @Schema(hidden = true)))})
   public ResponseEntity<List<ArticleView>> findAllArticles() {
     return ResponseEntity.ok(svc.findAll());
   }
 
+  @Override
   @GetMapping(value = "/findBy", produces = {"application/json"})
-  @Operation(summary = "filter articles by tag name", responses = {
-      @ApiResponse(responseCode = "OK", description = "200 Ok. There are some articles found by tag name",
-          content = @Content(array = @ArraySchema(schema = @Schema(implementation = ArticleView.class)))),
-      @ApiResponse(responseCode = "BAD_REQUEST", description = "400 Bad request. Missing tag name",
-          content = @Content(schema = @Schema(hidden = true))),
-      @ApiResponse(responseCode = "UNAUTHORIZED", description = "401 Unauthorized. Missing jwt token",
-          content = @Content(schema = @Schema(hidden = true))),
-      @ApiResponse(responseCode = "NOT_FOUND", description = "404 Not Found. No articles found by given tag name",
-          content = @Content(schema = @Schema(hidden = true)))})
-  public ResponseEntity<List<ArticleView>> findArticlesByTagNames(
-      @Parameter(explode = Explode.TRUE,
-          name = "tags",
-          description = "Collection of tag names",
-          in = ParameterIn.QUERY,
-          required = true,
-          style = ParameterStyle.FORM)
-      @Valid
-      @RequestParam Collection<String> tags) {
+  public ResponseEntity<List<ArticleView>> findArticlesByTagNames(@Valid @RequestParam Collection<String> tags) {
     if (CollectionUtils.isEmpty(tags)) {
       log.warn("empty tag request param");
       return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
@@ -82,21 +61,9 @@ public class ArticleController {
     return ResponseEntity.ok(result);
   }
 
+  @Override
   @PostMapping(consumes = {"application/json"})
-  @Operation(summary = "post new article", responses = {
-      @ApiResponse(responseCode = "CREATED", description = "201 Created. New article was stored",
-          content = @Content(schema = @Schema(implementation = ArticleView.class))),
-      @ApiResponse(responseCode = "BAD_REQUEST", description = "400 Bad request. For example missing or empty url",
-          content = @Content(schema = @Schema(hidden = true))),
-      @ApiResponse(responseCode = "UNAUTHORIZED", description = "401 Unauthorized. Missing jwt token",
-          content = @Content(schema = @Schema(hidden = true))),
-      @ApiResponse(responseCode = "CONFLICT", description = "409 Conflict. Article already exists",
-          content = @Content(schema = @Schema(hidden = true)))}
-  )
-  public ResponseEntity<?> postArticle(@Valid @RequestBody
-                                       @io.swagger.v3.oas.annotations.parameters.RequestBody(content =
-                                       @Content(schema = @Schema(implementation = ArticleView.class)))
-                                       ArticleForm request) {
+  public ResponseEntity<?> postArticle(@Valid @RequestBody ArticleForm request) {
     try {
       this.svc.addArticle(request);
       return new ResponseEntity<>(HttpStatus.CREATED);
